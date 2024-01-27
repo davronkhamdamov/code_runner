2f0b4e3239a83492c2047bc76676c05a20c1e2b2
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-01-27 21:39:20 +0500

    added C execute

--- controller/code_runner.js
+++ controller/code_runner.js
@@ -1,5 +1,7 @@
 const { exec } = require("child_process");
 const fs = require("fs");
+const { generateFile } = require("../lib/generateFile");
+const { executeC } = require("../lib/execute");
 
 function runJavaScriptCode(file_path) {
     try {
@@ -16,23 +18,31 @@ function runJavaScriptCode(file_path) {
     }
 }
 
-function run_code(req, res) {
-    const codeToRun = req.body.code;
-    if (!codeToRun) {
-        return res
-            .status(400)
-            .json({ error: "Code is required in the request body." });
+async function run_code(req, res) {
+    const { format = "cpp", code } = req.body;
+    if (!code) {
+        return res.status(400).json({
+            success: false,
+            error: "Code is required in the request body.",
+        });
     }
-    fs.writeFile("./controller/code/test.py", codeToRun, async (error) => {
-        if (error) throw new Error("Cannot write a file");
-        try {
-            const output = await runJavaScriptCode("./controller/code/test.py");
-            res.send({ result: output });
-            fs.writeFileSync("./controller/code/test.py", "");
-        } catch (error) {
-            res.status(500).send({ message: error });
-        }
-    });
+    try {
+        const filePath = generateFile(format, code);
+        const output = await executeC(filePath);
+        res.send({ output });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    // fs.writeFile("./controller/code/test.py", codeToRun, async (error) => {
+    //     if (error) throw new Error("Cannot write a file");
+    //     try {
+    //         const output = await runJavaScriptCode("./controller/code/test.py");
+    //         res.send({ result: output });
+    //         fs.writeFileSync("./controller/code/test.py", "");
+    //     } catch (error) {
+    //         res.status(500).send({ message: error });
+    //     }
+    // });
 }
 
 module.exports = run_code;
